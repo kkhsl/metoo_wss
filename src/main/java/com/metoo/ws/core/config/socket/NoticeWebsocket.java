d99a156a0b60a42ba27a769dfc1feecf846b322c
460751446@qq.com <460751446@qq.com>
2022-12-21 22:51:47 +0800

    master-idea-commit

--- src/main/java/com/metoo/ws/core/config/socket/NoticeWebsocket.java
+++ src/main/java/com/metoo/ws/core/config/socket/NoticeWebsocket.java
@@ -411,6 +411,6 @@ public class NoticeWebsocket {
     }
 
     public static void main(String[] args) {
-        System.out.println("master-update-195");
+        System.out.println("master-idea-commit");
     }
 }
