230bc75243dbd9d892f3d17876eab939ce6406bf
460751446@qq.com <460751446@qq.com>
2022-12-22 21:42:24 +0800

    idea-bch：测试本地仓库合并冲突

--- src/main/java/com/metoo/ws/core/config/socket/NoticeWebsocket.java
+++ src/main/java/com/metoo/ws/core/config/socket/NoticeWebsocket.java
@@ -412,7 +412,7 @@ public class NoticeWebsocket {
 
     public static void main(String[] args) {
         System.out.println("idea-bch:master-idea-commit");
-        System.out.println("master:测试合并分支，冲突解决");
+        System.out.println("master:测试合并分支，冲突解决1");
     }
 
 }
